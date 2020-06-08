769ff67ceecaecf2cc8e9efa64e56112f60fd312
Yona Appletree <yona@concentricsky.com>
2020-06-08 10:49:24 -0700

    Updated script to submit entries sequentially, rather than simultaneously, as that no longer works due to some change in TimeClock's form handling logic.

--- ical-timeclock.js
+++ ical-timeclock.js
@@ -24,7 +24,7 @@ function showDialog() {
 	cancelButton.innerText = "Cancel";
 	textarea.style.display = "block";
 	cancelButton.style.display = "inline-block";
-	cancelButton.type="button";
+	cancelButton.type = "button";
 	form.appendChild(cancelButton);
 	cancelButton.onclick = () => {
 		form.remove();
@@ -38,53 +38,81 @@ function showDialog() {
 	form.style.background = "gray";
 	form.style.padding = "1em";
 	form.style.zIndex = 1000;
+
 	form.onsubmit = (ev) => {
 		ev.preventDefault();
 		ev.stopPropagation();
 		form.remove();
+
 		fillFromCalendarInput(textarea.value);
 	};
+
 	document.body.appendChild(form);
 	textarea.focus();
 }
 
 function fillFromCalendarInput(input) {
-	if (!input || !input.trim().length) { return; }
-	const results = input.split("\n\n").map(entry => ({ entry: entry, error: submitTimeEntry(entry, true) }));
+	if (!input || !input.trim().length) {
+		return;
+	}
+
+	const results = input.split("\n\n").map(entry => ({entry: entry, error: submitTimeEntry(entry, true)}));
 	const errors = results.filter(r => !!r.error);
+
 	if (errors.length) {
 		alert("Errors found: \n" + errors.map(r => r.entry + "\n!! " + r.error + " !!").join("\n\n"));
 	} else {
-		Promise.all(results.map(r => submitTimeEntry(r.entry, false))).then(() => window.location.reload(),
-			errors => alert("Some entries failed to submit:\n" + errors.join("\n\n")))
+		// We used to fire off all requests simultaneously, but a change to timeclock caused that break, submitting
+		// only the first entry. Probably some sort of onsubmit handler delaying things. In any case, there are
+		// probably quicker ways of handling it, but this is more sure-fire.
+		(async () => {
+			for (let result of results) {
+				await submitTimeEntry(result.entry, false);
+			}
+		})().then(
+			() => {
+				window.location.reload();
+			},
+			errors => alert("Some entries failed to submit:\n" + errors.join("\n\n"))
+		);
 	}
 }
 
 function submitTimeEntry(input, dryrun) {
-	if (!input || !input.trim().length) { return; }
-	let [ description, timeString ] = input.trim().replace(/[\s\r\n]+/g, " ").split(/ Scheduled: /i);
+	if (!input || !input.trim().length) {
+		return;
+	}
+
+	let [description, timeString] = input.trim().replace(/[\s\r\n]+/g, " ").split(/ Scheduled: /i);
 	const ticketMatch = description.match(/^\s*([a-zA-Z0-9\-]+)\s*:/);
 	const projectActivityMatch = description.match(/#\s*([^/]*)\s*(?:\/\s*(.*)\s*)?$/);
 	description = description.replace(/#.*$/, "");
 
-	/* Handle project pattern */
-	if (!projectActivityMatch) { return "No project tag"; }
+	// Handle project pattern
+	if (!projectActivityMatch) {
+		return "No project tag";
+	}
+
 	const desiredProjectOption = $("#project option").toArray().find(
-		o => o.text.toLowerCase().indexOf(projectActivityMatch[ 1 ].toLowerCase().trim()) >= 0);
+		o => o.text.toLowerCase().indexOf(projectActivityMatch[1].toLowerCase().trim()) >= 0);
+
 	if (desiredProjectOption) {
 		$("#project").val(desiredProjectOption.value);
 		$("#project").change();
-	} else {return "Project tag #" + projectActivityMatch[ 1 ] + " did not match any projects";}
-	const [ dateStr, startStr, endStr ] = timeString.split(/\s*at\s*|\s*to\s*/g);
+	} else {
+		return "Project tag #" + projectActivityMatch[1] + " did not match any projects";
+	}
 
-	/* Add ticket number */
-	$("#ticket").val(ticketMatch ? ticketMatch[ 1 ] : "");
+	const [dateStr, startStr, endStr] = timeString.split(/\s*at\s*|\s*to\s*/g);
 
-	/* Handle activity */
+	// Add ticket number
+	$("#ticket").val(ticketMatch ? ticketMatch[1] : "");
+
+	// Handle activity
 	let desiredActivityOption;
-	if (projectActivityMatch[ 2 ]) {
+	if (projectActivityMatch[2]) {
 		desiredActivityOption = $("#activity option").toArray().find(
-			o => o.text.toLowerCase().indexOf(projectActivityMatch[ 2 ].toLowerCase().trim()) >= 0);
+			o => o.text.toLowerCase().indexOf(projectActivityMatch[2].toLowerCase().trim()) >= 0);
 	} else {
 		const activityPattern = !!description.match(/meeting|planning|storytime|review|retro/i)
 			? /meeting/i
@@ -94,11 +122,12 @@ function submitTimeEntry(input, dryrun) {
 
 	if (desiredActivityOption) {
 		$("#activity").val(desiredActivityOption.value);
+
+		// Fire change event so the UI is updated
 		$("#activity").change();
-		/* Fire change event so the UI is updated */
 	} else {
-		if (projectActivityMatch[ 2 ]) {
-			return "Unable to find correct activity for: " + projectActivityMatch[ 2 ];
+		if (projectActivityMatch[2]) {
+			return "Unable to find correct activity for: " + projectActivityMatch[2];
 		} else {
 			return "Unable to find correct activity."
 		}
@@ -109,15 +138,21 @@ function submitTimeEntry(input, dryrun) {
 	$("#startTime").val(startStr);
 	$("#endTime").val(endStr);
 
-	if (!dryrun) {
+	if (! dryrun) {
 		const iframe = document.createElement("iframe");
 		iframe.id = iframe.name = "iframe-" + Math.random();
 		document.body.appendChild(iframe);
-		$("#endTime")[ 0 ].form.target = iframe.id;
-		$("#endTime")[ 0 ].form.submit();
+
+		$("#endTime")[0].form.target = iframe.id;
+		$("#endTime")[0].form.submit();
+
 		return new Promise((resolve, reject) => {
 			iframe.onload = () => resolve(input);
 			iframe.onerror = () => reject(input);
 		});
 	}
+}
+
+function timeoutPromise(timeout) {
+	return new Promise(resolve => setTimeout(resolve, timeout));
 }
